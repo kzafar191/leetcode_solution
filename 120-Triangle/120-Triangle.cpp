f863448ca64807e2e5ccfe4ca8c0d63a708e2a82
Kashan Zafar Ansari <kashanz191@gmail.com>
2026-06-05 17:10:16 +0530

    Create 120-Triangle

---
+++ 120-Triangle/120-Triangle.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/6/2026, 5:10:16 pm
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        if (n == 0) {
+            return 0;
+        }
+
+        vector<int> dp = triangle.back();
+
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j <= i; ++j) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+};
