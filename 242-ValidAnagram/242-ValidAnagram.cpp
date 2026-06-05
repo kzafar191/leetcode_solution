f15306472f47f9e101f3c4c00e8f29be7507069e
Kashan Zafar Ansari <kashanz191@gmail.com>
2026-06-05 17:09:44 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,10 @@
+// Last updated: 5/6/2026, 5:09:43 pm
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        else return false;
+    }
+};
