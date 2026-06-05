ab200584d4f84194b44c7590917f5acb9d868d0d
Kashan Zafar Ansari <kashanz191@gmail.com>
2026-06-05 17:09:56 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/6/2026, 5:09:55 pm
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n= nums.size();
+        vector <int> dp(n+1);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;++i){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[n];
+    }
+};
